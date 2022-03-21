a8fcf121ddf894b39b0ffc5d68ccc8466c621193
Dave <wenyu.dai@qq.com>
2022-03-21 12:22:11 +0800

    更新

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -39,8 +39,17 @@ export const downloadFile = (href: string) => {
   aEle.remove();
 };
 
-export const getUrlParams = (paramsStr?: string) => {
-  const searchParams = new URLSearchParams(paramsStr || window.location.search);
+export const getUrlParams = (str?: string) => {
+  let paramsStr = window.location.search;
+  if (typeof str === 'string') {
+    if (str.startsWith('http')) {
+      const url = new URL(str);
+      paramsStr = url.search;
+    } else {
+      paramsStr = str;
+    }
+  }
+  const searchParams = new URLSearchParams(paramsStr);
   const params: any = {};
   for (let v of searchParams.entries()) {
     params[v[0]] = v[1];
